c7a488af265f80d2beefb82f515c4ebefd7797db
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2024-11-15 11:18:12 +0530

    Now Update the Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,32 @@
 package com.driver;
 
 public class Main {
-    public class Product { // Capitalize class name to match naming conventions
+    // Task 1: Create a class Product inside Main class
+    public static class Product {
 
+        // Task 3: Create a method with the following definition
         public int product(int x, int y) {
             return x * y;
         }
 
+        // Task 4: Create an overloaded method with the following definition
         public int product(int x, int y, int z) {
             return x * y * z;
         }
 
+        // Task 5: Create another overloaded method with the following definition
         public double product(double x, double y) {
             return x * y;
         }
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
-        Product p = main.new Product(); // Correct way to instantiate inner class
+        // Task 2: Create an object of Product in the main function called p
+        Product p = new Product();
 
-        System.out.println(p.product(4, 4));
-        System.out.println(p.product(3, 2, 3));
-        System.out.println(p.product(2.3, 3.2));
+        // Calling each overloaded method and printing the result
+        System.out.println(p.product(4,6));       // Calls product(int, int)
+        System.out.println(p.product(3, 2, 3));    // Calls product(int, int, int)
+        System.out.println(p.product(2.3, 3.2));   // Calls product(double, double)
     }
 }
